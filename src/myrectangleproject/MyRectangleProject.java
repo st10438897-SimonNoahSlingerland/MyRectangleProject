372d188191fd4d9b844c76b08aac8c7d7faa9202
st10438897-SimonNoahSlingerland <st10438897@vcconnect.edu.za>
2024-03-19 13:32:44 +0200

    Committing to master
    Committing to master

---
+++ src/myrectangleproject/MyRectangleProject.java
@@ -0,0 +1,107 @@
+package myrectangleproject;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Amber Bruil <abruil@varsitycollege.co.za>
+ */
+public class MyRectangleProject
+{
+    // Global variables
+    private static double length;
+    private static double width;
+    private static double areaResult;
+    private static double perResult;
+    private static Rectangle rectangleClass = new Rectangle();
+    
+    //--------------------------------------------------------------------------
+    // Main method
+    public static void main(String[] args)
+    {
+        // Start the program with getting the user's input method
+        GetUserInput();
+    }
+    
+    //--------------------------------------------------------------------------
+    // Method to retrieve the width and length from the user's input
+    private static void GetUserInput() 
+    {
+        // Using JOptionPane for GUI input (length)
+        String inputLength = JOptionPane.showInputDialog("Enter the length of "
+                                                            + "the rectangle:");
+        length = Double.parseDouble(inputLength);
+
+        // Using JOptionPane for GUI input (width)
+        String inputWidth = JOptionPane.showInputDialog("Enter the width of the "
+                                                                + "rectangle:");
+        width = Double.parseDouble(inputWidth);
+        
+        // Call method to proceed with creating a rectangle object
+        CreateRectangleInstance();
+    }
+    
+    //--------------------------------------------------------------------------
+    // Method to create the Rectangle Object and pass the length and width
+    private static void CreateRectangleInstance() 
+    {
+        // Populating the custom constructor 
+        rectangleClass = new Rectangle(length, width);
+        
+        // Once the attributes have been populated, call the next method to
+        // make the calculations done in the Rectangle class 
+        GetCalculations();
+    }
+    
+    //--------------------------------------------------------------------------
+    // Method to call the calculation methods from the Rectangle class
+    private static void GetCalculations() 
+    {
+        areaResult = rectangleClass.GetArea();
+        perResult = rectangleClass.GetPerimeter();
+        
+        // Proceed to call the method that display the results
+        DisplayMessage();
+    }
+    
+    //--------------------------------------------------------------------------
+    // Method to display the results message
+    private static void DisplayMessage() 
+    {
+        // Green output
+        System.out.println("\u001B[32mThe area of the rectangle: " + areaResult);
+        
+        // Green output
+        System.out.println("\u001B[32mThe perimeter of the rectangle: " + perResult);
+        
+        // Proceed to the next method 
+        Restart();
+    }
+    
+    //--------------------------------------------------------------------------
+    // Method to ask user if they'd like to restart
+    private static void Restart() 
+    {
+        // Creating a new scanner class
+        Scanner scanner = new Scanner(System.in);
+        
+        // Ask user if they would like to make a new calculation
+        System.out.print("Would you like to make " + "a new calculation (Y/N)? ");
+        String answer = scanner.nextLine();
+        
+        // Using an if statement to check whether the user wants to restart
+        if (answer.equals("y") || answer.equals("Y")) 
+        {
+            // User wants to restart so call the method to retrieve a new 
+            // input for the width and length
+            GetUserInput();
+        } 
+        else 
+        {
+            // Exit the program
+            System.exit(0);
+        }
+    }   
+ // ----------------------- 0oo0o0 End of File 0o0oo0 --------------------------    
+}
---
+++ src/myrectangleproject/Rectangle.java
@@ -0,0 +1,82 @@
+package myrectangleproject;
+
+/**
+ * 
+ * @author Amber Bruil <abruil@varsitycollege.co.za>
+ */
+public class Rectangle {
+
+    // Class fields
+    private double length;
+    private double width;
+    private double area;
+    private double perimeter;
+    
+    // Default constructor
+    public Rectangle()
+    {
+    }
+
+    // Custom constructor
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    // ----------------------- Getters and Setters -----------------------------
+    public double getLength()
+    {
+        return length;
+    }
+
+    public void setLength(double length)
+    {
+        this.length = length;
+    }
+
+    public double getWidth()
+    {
+        return width;
+    }
+
+    public void setWidth(double width)
+    {
+        this.width = width;
+    }
+
+    public double getArea()
+    {
+        return area;
+    }
+
+    public void setArea(double area)
+    {
+        this.area = area;
+    }
+
+    public double getPerimeter()
+    {
+        return perimeter;
+    }
+
+    public void setPerimeter(double perimeter)
+    {
+        this.perimeter = perimeter;
+    }
+    // ------------------ End of Getters and Setters ---------------------------
+    
+    // Method to return calculation of area
+    public double GetArea() 
+    {
+        return this.area = length * width;
+    } 
+    
+    // Method to return calculation of perimeter
+    public double GetPerimeter() 
+    {
+        return this.perimeter = 2 * (length + width);
+    }
+    
+ // ----------------------- 0oo0o0 End of File 0o0oo0 -------------------------- 
+}
